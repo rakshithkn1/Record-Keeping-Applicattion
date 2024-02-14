30b0cd73f80766da584ddf23ebd3b387bc88015c
rakshithkn1 <rakshithkn2001@gmail.com>
2024-02-14 21:54:27 +0530

    upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Header from './componenets/header'
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import AddIcon from '@mui/icons-material/Add';
+import { useState } from 'react';
+// import { Field } from './Field';
 
 function App() {
+const[name,setName]=useState('')
+
+const[email,setemail]=useState('')
+const[data,setdata]=useState([])
+
+const removeitem=(ind)=>{
+
+let arr=data
+arr.splice(ind,1)
+setdata([...arr])
+
+
+ 
+}
+
+
+const adddata=()=>{
+  setdata([
+    ...data,
+    {
+    name,
+    email
+  }])
+  setName('')
+  setemail('')
+}
+
+console.log(name);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className="app">
+    <Header></Header>
+    <div className='text'>
+    <Stack spacing={2} direction="row">
+    < TextField value={name} 
+    id="outlined-basic"
+     label="Name" 
+     onChange={(e)=>setName(e.target.value)}
+     variant="outlined" 
+     style={{margin:'2px'}} />
+    < TextField value={email} 
+   onChange={(e)=>setemail(e.target.value)}
+    id="outlined-basic" 
+    label="Email" 
+    variant="outlined" 
+    style={{margin:'2px'}} />
+    <Button onClick={adddata} variant="contained" style={{backgroundColor:'red'}}>  <AddIcon/></Button>
+    </Stack>
+  
+
     </div>
+
+    <div className='data'>
+             <div className='data_1'>
+               <h4> Name</h4>
+               <h4> Email</h4>
+               <h4 >Remove</h4>
+             </div>
+             {
+            data.map((ele,ind)=>{
+              return(
+                <div className='data_1'>
+                <h4> {ele.name}</h4>
+                <h4> {ele.email}</h4>
+                <button onClick={()=>removeitem(ind)}>Remove</button>
+              </div>
+              )
+
+            })
+          }
+
+          </div>
+        
+
+   
+      {
+        // data.map(({Name,Email})=>{
+        //   return(
+        //     <div className='data'>
+        //     <div className='data_1'>
+        //       <h4> Name:{Name}</h4>
+        //       <h4> Email:{Email}</h4>
+        //       <button onClick={}>Remove</button>
+        //     </div>
+        //     </div>
+        //   )
+        
+
+        // })
+
+        // data.map((obj ,ind)=>{
+        //   return(
+        //     <Field
+        //     Name={obj.Name}
+        //     Email={obj.Email}
+        //     />
+        
+
+        //   )
+
+        // })
+      }
+   </div>
   );
+
 }
 
 export default App;
---
+++ src/componenets/header.js
@@ -0,0 +1,8 @@
+const Header=()=>{
+    return(
+        <div className="header">
+          <h1>Record Keeping appliacaion</h1>  
+        </div>
+    )
+}
+export default  Header
